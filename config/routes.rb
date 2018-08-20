f0b546dd48dc89b00d492875ed25f60bc22ca5f4
Julien Mottet <mottet.julien123@gmail.com>
2018-08-21 01:27:19 +0200

    add heroku settings...its working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:index]
   resources :attractions, only: [:show, :index, :new, :create]
   resources :bookings, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
