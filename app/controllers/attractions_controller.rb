4261b92269759a79029cbc3a937816c31c8124ed
Nicolas Meyer <myr.nicolas@gmail.com>
2018-08-21 11:12:58 +0200

    pundit first commit

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,22 +1,40 @@
 class AttractionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_attraction, only: [:show, :edit, :update]
 
   def index
-    @attractions = Attraction.all
+    @attractions = policy_scope(Attraction).order(created_at: :desc)
   end
 
   def show
   end
 
-  def create
+  def new
+    @attraction = Attraction.new
+    authorize @attraction
   end
 
-  def new
+  def create
+
   end
 
   def edit
   end
 
   def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_attraction
+    @attraction = Attraction.find(params[:id])
+    authorize @attraction
+  end
+
+  def params_attraction
+    params.require(:attraction).permit(:name, :attraction_type, :playersmax, :description, :price, :location)
   end
 end
