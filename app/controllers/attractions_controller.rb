2ac4f450a1a762772b1d2f4c0849481e77c8203b
Julien Mottet <mottet.julien123@gmail.com>
2018-08-21 11:56:36 +0200

    create edit delte attraction

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,6 @@
 class AttractionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_attraction, only: [:show, :edit, :update, :destroy]
 
   def index
     @attractions = Attraction.all
@@ -9,14 +10,44 @@ class AttractionsController < ApplicationController
   end
 
   def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      render :new
+    end
   end
 
   def new
+    @attraction = Attraction.new
   end
 
   def edit
+
   end
 
   def update
+    if @attraction.update(attraction_params)
+      redirect_to attraction_path(@attraction)
+    else
+      render :edit
+    end
   end
+
+  def destroy
+    @attraction.destroy
+    redirect_to attractions_path
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :attraction_type, :playersmax)
+  end
+
+  def set_attraction
+    @attraction = Attraction.find(params[:id])
+  end
+
+
 end
