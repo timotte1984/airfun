5835927e4505de39c59a945f9a4a902fd43dcace
Nicolas Meyer <myr.nicolas@gmail.com>
2018-08-24 11:27:53 +0200

    autocomplete

--- app/javascript/maps/autocomplete.js
+++ app/javascript/maps/autocomplete.js
@@ -1,6 +1,6 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var flatAddress = document.getElementById('flat_address');
+    var flatAddress = document.getElementById('attraction_location');
 
     if (flatAddress) {
       var autocomplete = new google.maps.places.Autocomplete(flatAddress, { types: [ 'geocode' ] });
