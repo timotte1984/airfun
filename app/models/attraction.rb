51a4530b889df3f56bf8b4f58ff13ebbd0c912db
Nicolas Meyer <myr.nicolas@gmail.com>
2018-08-24 16:38:31 +0200

    deletion fixed

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,5 +1,5 @@
 class Attraction < ApplicationRecord
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :availabilities
   belongs_to :user
   validates :name, presence: true, allow_blank: false
